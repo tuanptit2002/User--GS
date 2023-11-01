7fa0b1897a00ae8fe2e1e7f93653b76db86dea9f
tientato2002 <tientato2002@gmail.com>
2023-11-01 12:47:51 +0700

    test
    Signed-off-by: tientato2002 <tientato2002@gmail.com>

--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RegisterRoutingModule } from './register-routing.module';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     RegisterRoutingModule
